ea516c4d65ea7772bac72d6085ae184d70ab75ad
Houssam EL Mansouri <31924302+Chu3laMan@users.noreply.github.com>
2021-09-26 17:54:42 +0100

    Update PortfolioController.java

--- src/main/java/net/codejava/controller/PortfolioController.java
+++ src/main/java/net/codejava/controller/PortfolioController.java
@@ -24,7 +24,7 @@ public class PortfolioController {
 	@Autowired
 	private PortfolioService portfolioService;
 
-	
+	//getAllPortfolios() method used to fetch all Portfolios that has been registered in the VoompDB database
 	@RequestMapping(value = "/")
 	public String getAllPortfolios(Model model) {
 		model.addAttribute("portfolios", portfolioService.getAllPortfolios());
@@ -32,7 +32,7 @@ public class PortfolioController {
 		return "Portfolio";
 	}
 	
-	
+	//addNewPortfolio method register a new Portfolio that has been entered by the end-user 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String addNewPortfolio(@ModelAttribute("newPortfolio") Portfolio portfolio) {
 		portfolioService.addPortfolio(portfolio);
@@ -50,13 +50,14 @@ public class PortfolioController {
 	
 	//-----------------------------------------------------------------------------------------------------------------
 	
+	//updateAndsave() used to upate an exist record that has been registered in VOOMP database
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	public String updateAndsave(@ModelAttribute("portfolio") Portfolio p) {
 		portfolioService.update(p);
 		return "redirect:/";
 	}
 	
-	
+	//this method used to edit an exist portfolio
 	@RequestMapping(value = "/edit")
 	public ModelAndView getPortfolioById(@RequestParam("id") int portfolioId) {
 		ModelAndView model = new ModelAndView("editPortfolio");
@@ -65,6 +66,7 @@ public class PortfolioController {
 		return model;
 	}
 	
+	//deletePortfolio() method used to delete a Portfolio from the VoompDB database
 	@RequestMapping(value = "/delete/{id}")
 	public String deletePortfolio(@RequestParam long id) {
 		portfolioService.delete(id);
